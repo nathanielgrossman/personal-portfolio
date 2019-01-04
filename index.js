88d6bfaef1c4270e1d9236b6f071a850f8275297
nathanielgrossman <nategrossman@mac.com>
2019-01-03 23:12:07 -0800

    added function to parse rgba string to rgb

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ axios.get('https://kboykyzml8.execute-api.us-west-2.amazonaws.com/prod/get-rando
     .then(image => {
         let markup = `<picture class="picture"><source type="image/webp" class="picture" srcset="https://s3-us-west-1.amazonaws.com/18a58t9c/webp/${image._id}.webp"><img class="picture" alt="${image.original}" src="https://s3-us-west-1.amazonaws.com/18a58t9c/jpg/${image._id}.jpg"></picture>`;
         let contrast = invert(image.color, true);
-        
+
         let links = document.getElementsByTagName('a')
         for (let i in links) {
             if (contrast.length === 17) {
@@ -13,7 +13,7 @@ axios.get('https://kboykyzml8.execute-api.us-west-2.amazonaws.com/prod/get-rando
             }
         }
 
-        document.body.style.background = image.color;
+        document.body.style.background = parseRGB(image.color);
         document.body.style.color = contrast;
         document.getElementById('image').innerHTML = markup;
         console.log(image.color);
@@ -23,8 +23,13 @@ axios.get('https://kboykyzml8.execute-api.us-west-2.amazonaws.com/prod/get-rando
 function invert(rgb, bw) {
     let channels = rgb.substring(4, rgb.length - 1).split(", ").map(channel => parseInt(channel))
     let a = channels.pop();
-    if (bw) return (channels[0] * 0.299 + channels[1] * 0.587 + channels[2] * 0.114) > 186 ? "rgb(0, 0, 0, 255)" : "rgb(255, 255, 255, 255)"
+    if (bw) return (channels[0] * 0.299 + channels[1] * 0.587 + channels[2] * 0.114) > 186 ? "rgb(0, 0, 0)" : "rgb(255, 255, 255)"
     let inverted = channels.map(channel => 255 - channel);
-    inverted.push(a)
     return `rgb(${inverted.join(', ')})`
+}
+
+function parseRGB(rgba) {
+  let channels = rgb.substring(4, rgb.length - 1).split(", ").map(channel => parseInt(channel))
+    let a = channels.pop();
+    return `rgb(${channels.join(', ')})`
 }
